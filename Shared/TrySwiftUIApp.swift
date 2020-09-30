049af1b31535b5b630a6494f5d88d896ad368b5c
ttdp <tiantong.dev@gmail.com>
2020-09-30 09:48:17 +0800

    Initial Commit

---
+++ Shared/TrySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrySwiftUIApp.swift
+//  Shared
+//
+//  Created by Tian Tong on 9/30/20.
+//
+
+import SwiftUI
+
+@main
+struct TrySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
